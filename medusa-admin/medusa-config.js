ffec9a7b84ec188a83bf1e336d8b8d49acb8246a
Am.ChrisKE <dev.humblechris254@gmail.com>
2023-08-02 18:19:23 +0000

    added lock file

--- medusa-admin/medusa-config.js
+++ medusa-admin/medusa-config.js
@@ -28,8 +28,17 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+// const DATABASE_URL =
+//   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_DATABASE = process.env.DB_DATABASE
+
+const DATABASE_URL = 
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -79,24 +88,27 @@ const projectConfig = {
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
-// module.exports = {
-//   projectConfig,
-//   plugins,
-//   modules,
-// };
-
 module.exports = {
   projectConfig: {
-    redis_url: REDIS_URL,
-    database_url: DATABASE_URL,
-    database_type: "postgres",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
-    database_extra:
-      process.env.NODE_ENV !== "development"
-        ? { ssl: { rejectUnauthorized: false } }
-        : {},
+    // ...
+    database_extra: { ssl: { rejectUnauthorized: false } },
   },
   plugins,
   modules,
-}
+};
+
+// module.exports = {
+//   projectConfig: {
+//     redis_url: REDIS_URL,
+//     database_url: DATABASE_URL,
+//     database_type: "postgres",
+//     store_cors: STORE_CORS,
+//     admin_cors: ADMIN_CORS,
+//     database_extra:
+//       process.env.NODE_ENV !== "development"
+//         ? { ssl: { rejectUnauthorized: false } }
+//         : {},
+//   },
+//   plugins,
+//   modules,
+// }
