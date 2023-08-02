b0f8acca135c9ef768a47e1593a1737e5095675b
Am.ChrisKE <dev.humblechris254@gmail.com>
2023-08-02 15:30:17 +0000

    updated the SEO

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -5,12 +5,12 @@ useHead({
   },
   meta: [
     { property: 'og:title', content: `Panto` },
-    { name: 'description', content: 'Panto is a Ecommerce Site that sells furnitures, its built with Nuxt 3, Medusa and TailwindCSS.' },
-    { property: 'og:description', content: 'Panto is a Ecommerce Site that sells furnitures, its built with Nuxt 3, Medusa and TailwindCSS.' },
-    { property: 'og:url', content: 'https://jadoo-ke.vercel.app/' },
+    { name: 'description', content: 'Panto is a Ecommerce Site that sells furniture, its built with Nuxt 3, Medusa and Tailwind CSS.' },
+    { property: 'og:description', content: 'Panto is a Ecommerce Site that sells furniture, its built with Nuxt 3, Medusa and Tailwind CSS.' },
+    { property: 'og:url', content: 'https://panto-ke.vercel.app/' },
     { name: 'twitter:title', content: 'Panto' },
-    { name: 'twitter:description', content: 'Panto is a Ecommerce Site that sells furnitures, its built with Nuxt 3, Medusa and TailwindCSS.' },
-    { name: 'twitter:site', content: 'https://jadoo-ke.vercel.app/' }
+    { name: 'twitter:description', content: 'Panto is a Ecommerce Site that sells furniture, its built with Nuxt 3, Medusa and Tailwind CSS.' },
+    { name: 'twitter:site', content: 'https://panto-ke.vercel.app/' }
   ],
   title: `Panto`,
 })
