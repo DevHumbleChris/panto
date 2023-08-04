717c1be74a790ca42d9023fd845c56e23a598fd5
Am.ChrisKE <dev.humblechris254@gmail.com>
2023-08-04 09:48:53 +0000

    made header be responsive

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "nuxt-icon"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     config: {
