b00683622dab03d73deab113cbe560055a0e8278
DevHumbleChris <dev.humblechris254@gmail.com>
2023-08-10 14:39:28 +0000

    redefined cors

--- medusa-admin/medusa-config.js
+++ medusa-admin/medusa-config.js
@@ -22,8 +22,10 @@ try {
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+// const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+const ADMIN_CORS = process.env.STORE_CORS || process.env.STORE_CORS_GITPOD;
+
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || process.env.STORE_CORS_GITPOD;
